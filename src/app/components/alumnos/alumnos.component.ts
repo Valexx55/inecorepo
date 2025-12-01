b9adaff7479425b26d44a33f6e7357fcf784c2b3
Val <valexx55@gmail.com>
2025-12-01 13:03:11 +0100

    estilos orientación

--- src/app/components/alumnos/alumnos.component.ts
+++ src/app/components/alumnos/alumnos.component.ts
@@ -27,6 +27,9 @@ export class AlumnosComponent  implements OnInit {
 
   listaAlumnosRx!: Array<Alumno>;
   loading!: HTMLIonLoadingElement;
+  headerOpacity = 1;
+
+  isPortrait:boolean = true
 
 
   observerAlumnos: Observer<Array<Alumno>> = {
@@ -54,9 +57,48 @@ export class AlumnosComponent  implements OnInit {
 
 
 
+  onScroll(event:any)
+  {
+    //calculamos una nueva opacidad según descendemos
+    console.log('onScroll');
+    let scrollTop = event.detail.scrollTop;
+    let opacity = 1 - scrollTop / 2000;
+    this.headerOpacity = Math.max(0.2, opacity);
+
+  }
+
+  //otros eventos que podría escuchar
+  //onScrollStart
+  //onScrollEnd //para paginar dinámicamente
+
+
   ngOnInit() {
     this.mostrarCargando();
     this.alumnoSerivce.getAlumnos().subscribe(this.observerAlumnos)
+    
+    //trabajo con la orientación
+
+    const consulta = window.matchMedia("(orientation: portrait)");
+
+    this.isPortrait =  consulta.matches;
+
+    console.log ('Estado inicial ' + this.isPortrait)
+
+    consulta.addEventListener("change" ,
+      (ev) => {
+        this.isPortrait = ev.matches;
+        if (this.isPortrait)
+        {
+          console.log('Pasó a vertical')
+          //alert('Pasó a vertical')
+        } else {
+          console.log('Pasó a horizontal')
+          //alert('Pasó a horizontal')
+        }
+      }
+    )
+
+  
   }
 
   async mostrarCargando() {
